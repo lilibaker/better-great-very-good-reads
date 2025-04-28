ee3a7770e6947639a396534c5d5e868fda714141
Lili Baker <lilijbaker@gmail.com>
2025-04-28 19:34:52 -0400

    Add book and reviews db

--- reader-radar/schema.sql
+++ reader-radar/schema.sql
@@ -18,3 +18,25 @@ CREATE TABLE post (
   body TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
+
+CREATE TABLE book (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author INTEGER NOT NULL,
+  isbn INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  original_language TEXT NOT NULL,
+  publisher TEXT NOT NULL,
+  publish_date TIMESTAMP NOT NULL,
+  page_length INTEGER NOT NULL,
+  genre TEXT NOT NULL
+);
+
+CREATE TABLE review (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  comment TEXT NOT NULL,
+  rating INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
+  book_id INTEGER NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id),
+  FOREIGN KEY (book_id) REFERENCES book (id)
+);
