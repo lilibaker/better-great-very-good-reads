61cca44233be1348019c237f8a1e231d682d5774
Kat Canavan <60857666+thegalaxykat@users.noreply.github.com>
2025-05-02 18:36:02 -0400

    Make hello world index page

--- reader-radar/__init__.py
+++ reader-radar/__init__.py
@@ -57,6 +57,7 @@ def create_app(test_config=None):
     # in another app, you might define a separate main index here with
     # app.route, while giving the blog blueprint a url_prefix, but for
     # the tutorial the blog will be the main index
-    app.add_url_rule("/", endpoint="index")
+    # app.add_url_rule("/", endpoint="index")
+    app.add_url_rule("/", endpoint="hello")  # this is temporary
 
     return app
