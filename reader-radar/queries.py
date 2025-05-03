dc9b796db7fcf4ff96e0c3469bf634751f851060
Kat Canavan <60857666+thegalaxykat@users.noreply.github.com>
2025-05-03 12:29:26 -0400

    Add queries

---
+++ reader-radar/queries.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+import click  # command line interface
+from flask import current_app
+from flask import g  # store data for current request
+
+# get book lists
+
+# get books
+
+# get ratings
+
+# get review text
+
+# get books by title or author
