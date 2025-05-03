094601358fdd44d6e5c6c5b1a79f3bb412f6e317
Kat Canavan <60857666+thegalaxykat@users.noreply.github.com>
2025-05-03 13:34:44 -0400

    Add get_book implementation

--- reader-radar/queries.py
+++ reader-radar/queries.py
@@ -1,15 +1,70 @@
 import sqlite3
 
-import click  # command line interface
-from flask import current_app
+from flask import Blueprint
+from flask import flash
 from flask import g  # store data for current request
+from flask import redirect
+from flask import render_template
+from flask import render_template_string
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
 
-# get book lists
+from .auth import login_required
+from .db import get_db
 
-# get books
+
+# init my lists
+# def render_my_lists(user_id):
+#   """Get books lists and books for my lists page."""
+#   book_lists = get_book_lists(user_id)
+#   books = get_user_books(user.id)
+#   return render_template("mylists.html", book_lists, books)
+
+
+# def get_book_list_books(list_name):
+#     """
+#     Get books for list given id.
+#     """
+#     books_lists = (
+#         get_db()
+#         .execute(
+#             "SELECT b.id, user_id, list_name"
+#             "FROM book_list b JOIN user u ON b.user_id = u.id"  #! change this if login logic knows user id
+#             "WHERE b.list_name = ?"(
+#                 list_name,
+#             ),
+#         )
+#         .fetchall()
+#     )
+
+#     return books_lists
+
+
+# def get_book_list_items()
+
+# get books in list
 
 # get ratings
 
 # get review text
 
 # get books by title or author
+
+
+# get book page
+def get_book(id):
+    """Get book by id"""
+    book = (
+        get_db()
+        .execute(
+            "SELECT b.id, title, author, isbn, page_length, synopsis"
+            " FROM books b"
+            " WHERE b.id = ? ",
+            (id,),
+        )
+        .fetchone()
+    )
+    if book is None:
+        abort(404, f"Book id {id} doesn't exist")
+    return book
