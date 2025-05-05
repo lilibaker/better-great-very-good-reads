b1cd3d8d4fcfb6a62b4778425fbb9ae9aa773cc2
Lili Baker <lilijbaker@gmail.com>
2025-05-05 16:45:27 -0400

    Add process change list

--- reader-radar/library.py
+++ reader-radar/library.py
@@ -31,3 +31,31 @@ def library():
         wish_list=wish_list,
         finished=finished,
     )
+
+
+@bp.route("/move_book/<int:list_id>/book_id", methods=["GET", "POST"])
+@login_required
+def move_book(list_id, book_id):
+    """Add book to new list"""
+    if request.method == "POST":
+        book_id = request.form["book_id"]
+        list_id = request.form["list_id"]
+        error = None
+
+        if not book_id or list_id:
+            error = "No book or list"
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "INSERT INTO book_list_items (list_id, book_id) VALUES (?, ?)",
+                (list_id, book_id),
+            )
+            db.commit()
+            return redirect(url_for("library"))
+
+    return render_template(
+        "library.html"
+    ) 
