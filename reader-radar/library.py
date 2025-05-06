85fb4ea50f71f4f62fc26886ee2db3de4b17cddf
Kat Canavan <60857666+thegalaxykat@users.noreply.github.com>
2025-05-05 21:52:40 -0400

    Add library list buttons to UI

--- reader-radar/library.py
+++ reader-radar/library.py
@@ -25,7 +25,7 @@ def library():
     wish_list = queries.get_book_list_books("Wish List", g.user["id"])
     finished = queries.get_book_list_books("Finished", g.user["id"])
     return render_template(
-        "test_library.html",
+        "library.html",
         current=current,
         unread=unread,
         wish_list=wish_list,
@@ -41,35 +41,27 @@ def move_book():
         book_id = request.form["book_id"]
         new_list_name = request.form["new_list_name"]
         old_list_name = request.form["old_list_name"]
-        error = None
         print("first one works")
         print(book_id)
         print(new_list_name)
         print(old_list_name)
 
-        # if not book_id or new_list_name:
-        #     error = "No book or list"
-        #     print("No book or list")
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            if old_list_name != "":
-                old_list_id = queries.get_book_list_id(old_list_name, g.user["id"])
-                db.execute(
-                    "DELETE FROM book_list_items WHERE book_id = ? AND book_list_id = ?",
-                    (book_id, old_list_id),
-                )
-            if new_list_name != "Remove Book":
-                new_list_id = queries.get_book_list_id(new_list_name, g.user["id"])
-                db.execute(
-                    "INSERT INTO book_list_items (book_list_id, book_id) VALUES (?, ?)",
-                    (new_list_id, book_id),
-                )
-            db.commit()
-            print("Book moved successfully")
-            return redirect(url_for("library.library"))
+        db = get_db()
+        if old_list_name != "":
+            old_list_id = queries.get_book_list_id(old_list_name, g.user["id"])
+            db.execute(
+                "DELETE FROM book_list_items WHERE book_id = ? AND book_list_id = ?",
+                (book_id, old_list_id),
+            )
+        if new_list_name != "Remove Book":
+            new_list_id = queries.get_book_list_id(new_list_name, g.user["id"])
+            db.execute(
+                "INSERT INTO book_list_items (book_list_id, book_id) VALUES (?, ?)",
+                (new_list_id, book_id),
+            )
+        db.commit()
+        print("Book moved successfully")
+        return redirect(url_for("library.library"))
 
     print("Error moving book")
     return redirect(url_for("library.library"))
